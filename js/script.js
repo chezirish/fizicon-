49cc4d3c3c478124d0638700c0335e1365314e31
Ivan <chezirishth@mail.ru>
2017-12-10 22:26:00 +0500

    make flex instead of floats

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $(function() {
+    // add jquery ui radio buttons
     $('.wrapper__radio').checkboxradio();
 
 
     var apiResponse = null;
-
+    //function for make api call
     function requestApi(){
         var data = {'data':''};
-        var url = '//api.qa.imumk.ru/api/mobilev1/update/';
+        var url = 'http://api.qa.imumk.ru/api/mobilev1/update/';
         
         $.ajax({
             type: "POST",
@@ -21,6 +22,7 @@ $(function() {
                 alert(response.errorMessage);
             } else {
                 apiResponse = response.items;
+                //if get response call render function which will render elements
                 render();
             }
         }
@@ -39,23 +41,25 @@ $(function() {
     var gen = document.getElementById('genre');
     var gra = document.getElementById('grade');
     var search = document.getElementById('search');
-    // бонусы баг
-    // адаптив баги
-    // ошибка popper
 
+    // if any input was changed make new render
     radios.addEventListener('click', render);
     sub.addEventListener('change', render);
     gen.addEventListener('change', render);
     gra.addEventListener('change', render);
     search.addEventListener('input', render);
 
+
+    // function for render elements with api data
     function render(){
-        var row__wrapper = document.getElementsByClassName("row__wrapper")[0];
-        if(row__wrapper){
-            row__wrapper.parentNode.removeChild(row__wrapper);    
+
+        // delete old elements
+        var apiItems = document.getElementsByClassName("api-items")[0];
+        if(apiItems){
+            apiItems.parentNode.removeChild(apiItems);    
         }
 
-
+        //get values from inputs
         var subjectVal = sub.value;
         var genreVal = gen.value;
         var gradeVal = gra.value;
@@ -64,16 +68,13 @@ $(function() {
 
         searchVal = searchVal.toLowerCase();
         searchVal = searchVal.replace(/\s/g, '');
-        // alert(searchVal)
 
-        row__wrapper = document.createElement('div');
-        row__wrapper.classList += ' row__wrapper';
-        document.getElementsByClassName('wrapper')[0].appendChild(row__wrapper);
+        //make wrapper for elements
+        flexContainer = document.createElement('div');
+        flexContainer.classList += ' flex-container api-items';
+        document.getElementsByClassName('wrapper')[0].appendChild(flexContainer);
 
-        var row = null;
-        var count = 0;
-        // console.log(apiResponse);
-
+        // go through apiResponse arr and render elements dependence of inputs values
         for(var i=0; i<apiResponse.length; i++){
             var item = apiResponse[i];
             if(subjectVal === 'Все предметы' || item.subject.indexOf(subjectVal)  > -1 ){
@@ -82,14 +83,9 @@ $(function() {
                         item.title = item.title.toLowerCase();
                         item.title = item.title.replace(/\s/g, '');
                         if(searchVal === '' || item.title.indexOf(searchVal) > -1 ){
-                            if(count % 5 === 0){
-                                row = document.createElement('div');
-                                row.className += " row";
-                                row__wrapper.appendChild(row);
-                            }
-                            
-                            var col = document.createElement('div');
-                            var col__content = document.createElement('div');
+
+                            // create elements
+                            var flex__content = document.createElement('div');
                             var figure = document.createElement('div');
                             var info = document.createElement('div');
                             var cover = document.createElement('img');
@@ -102,10 +98,9 @@ $(function() {
                             var a1 = document.createElement('a');
                             var a2 = document.createElement('a');
                             var a3 = document.createElement('a');
-                            
-                            col.classList += " col";
-                            col.classList += " colfix";
-                            col__content.classList += " col__content";
+
+                            //add classes for elements
+                            flex__content.classList += " flex__content";
                             figure.classList += " figure";
                             info.classList += " info";
                             cover.classList += " cover";
@@ -116,6 +111,7 @@ $(function() {
                             more.classList += " more";
                             buy.classList += " buy";
 
+                            //insert data from api arr in elements
                             cover.src = 'https://www.imumk.ru/covers/' +item.courseId+ '.png';
                             a1.href = '#';
                             a1.innerHTML = 'Попробовать';
@@ -129,10 +125,9 @@ $(function() {
                             grade.innerHTML = item.grade + ' класс';                        
                             genre.innerHTML = item.genre;                        
                             
-
-                            col.appendChild(col__content);
-                            col__content.appendChild(figure);
-                            col__content.appendChild(info);
+                            
+                            flex__content.appendChild(figure);
+                            flex__content.appendChild(info);
                             figure.appendChild(cover);                        
                             figure.appendChild(figure__modal);
                             figure__modal.appendChild(a1);
@@ -143,9 +138,7 @@ $(function() {
                             info.appendChild(buy);
                             more.appendChild(a2);
                             buy.appendChild(a3);
-                            row.appendChild(col);
-                            
-                            count++;
+                            flexContainer.appendChild(flex__content);
                         }    
                     } 
                 }
